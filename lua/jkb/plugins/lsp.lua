ab9f2331588344ebbe1d24d62c849711f10473b0
jkbSeven <Jacob202@protonmail.com>
2026-02-16 23:47:34 +0100

    chore: updated packages, updated treesitter config to accomodate breaking changes, removed unused stuff

--- lua/jkb/plugins/lsp.lua
+++ lua/jkb/plugins/lsp.lua
@@ -28,7 +28,6 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "gopls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
