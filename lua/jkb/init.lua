4377b721acac14295c84ab8dad91c2c51f9982a0
jkbSeven <Jacob202@protonmail.com>
2026-04-04 16:30:55 +0200

    debloated LSP config

--- lua/jkb/init.lua
+++ lua/jkb/init.lua
@@ -5,7 +5,6 @@ require("jkb.lazy_init")
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-local jkb_group = augroup('jkb', {})
 local yank_group = augroup('HighlightYank', {})
 
 autocmd('TextYankPost', {
@@ -17,18 +16,4 @@ autocmd('TextYankPost', {
             timeout = 40,
         })
     end,
-})
-
-autocmd('LspAttach', {
-    group = jkb_group,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-    end
 })
